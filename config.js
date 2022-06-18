dcde8af799d7e82f55be880380c7f4f5c4f770ba
beta2345 <107755477+beta2345@users.noreply.github.com>
2022-06-18 19:48:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "so much time",
-    introTitle: 'Phan Thị Yến Nhi',
+    titleWeb: "Thân Gửi",
+    introTitle: 'cậu gì đó ơi',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -12,7 +12,7 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu❤',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 12h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
